cad182e9f024539e70ec3c16b90f0e49135075cf
adam <ahanna@alumni.mines.edu>
2019-01-10 19:05:57 -0800

    added crypto/Hash type

--- common/crypto/crypto.go
+++ common/crypto/crypto.go
@@ -6,20 +6,29 @@ import (
 	"golang.org/x/crypto/blake2b"
 )
 
+// Hash ...
+type Hash [sha256.Size]uint8
+
+// Blake2b256Hash ...
+type Blake2b256Hash [blake2b.Size256]uint8
+
+// Blake2b512Hash ...
+type Blake2b512Hash [blake2b.Size]uint8
+
 // NewSHA256 ...
-func NewSHA256(data []byte) []byte {
+func NewSHA256(data []byte) Hash {
 	hash := sha256.Sum256(data)
-	return hash[:]
+	return hash
 }
 
 // NewBlake2b256 ...
-func NewBlake2b256(data []byte) []byte {
+func NewBlake2b256(data []byte) Blake2b256Hash {
 	hash := blake2b.Sum256(data)
-	return hash[:]
+	return hash
 }
 
 // NewBlake2b512 ...
-func NewBlake2b512(data []byte) []byte {
+func NewBlake2b512(data []byte) Blake2b512Hash {
 	hash := blake2b.Sum512(data)
-	return hash[:]
+	return hash
 }
--- common/triehash/trieroot.go
+++ common/triehash/trieroot.go
@@ -3,19 +3,20 @@ package triehash
 import (
 	"math/big"
 
+	"github.com/c3systems/go-substrate/common/crypto"
 	"github.com/c3systems/go-substrate/common/triecodec"
 	"github.com/c3systems/go-substrate/common/u8compact"
 )
 
 // TrieRoot creates a trie hash from the supplied pairs.
-func TrieRoot(input []*TriePair) []uint8 {
+func TrieRoot(input []*TriePair) crypto.Blake2b256Hash {
 	return triecodec.Hashing(
 		UnhashedTrie(input),
 	)
 }
 
 // TrieRootOrdered creates a trie hash from the supplied pairs.
-func TrieRootOrdered(input [][]uint8) []uint8 {
+func TrieRootOrdered(input [][]uint8) crypto.Blake2b256Hash {
 	var values []*TriePair
 	for index, value := range input {
 		values = append(values, &TriePair{
